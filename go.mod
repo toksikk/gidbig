469978bf139c1db1117ec48a46adc8af3bb2e174
Daniel Aberger <da@ixab.de>
2023-11-30 20:17:29 +0100

    feat(plugins): fuck go plugins and built them in as dependecy - seriously fuck go plugins

--- go.mod
+++ go.mod
@@ -1,24 +1,39 @@
 module github.com/toksikk/gidbig
 
-go 1.20
+go 1.21
 
 require (
 	github.com/bwmarrin/discordgo v0.27.1
 	github.com/dustin/go-humanize v1.0.1
-	github.com/gorilla/mux v1.8.0
-	github.com/gorilla/sessions v1.2.1
+	github.com/gorilla/mux v1.8.1
+	github.com/gorilla/sessions v1.2.2
 	github.com/simplesurance/go-ip-anonymizer v0.0.0-20200429124537-35a880f8e87d
-	golang.org/x/oauth2 v0.11.0
+	github.com/toksikk/gbp-eso v0.0.0-20231130184756-7a291afa95aa
+	github.com/toksikk/gbp-leetoclock v0.4.4
+	github.com/toksikk/gbp-stoll v0.0.0-20231130184730-256739403f29
+	github.com/toksikk/gbp-wttrin v0.1.2-0.20231130185041-33d0362c24da
+	golang.org/x/oauth2 v0.15.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.18 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	gorm.io/driver/sqlite v1.5.4 // indirect
+	gorm.io/gorm v1.25.5 // indirect
+)
+
+require (
 	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/gorilla/securecookie v1.1.1 // indirect
-	github.com/gorilla/websocket v1.5.0 // indirect
-	golang.org/x/crypto v0.12.0 // indirect
-	golang.org/x/net v0.14.0 // indirect
-	golang.org/x/sys v0.11.0 // indirect
-	google.golang.org/appengine v1.6.7 // indirect
+	github.com/gorilla/securecookie v1.1.2 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
+	github.com/toksikk/gbp-coffee v0.0.0-20231130185010-31ec6e535b0b
+	github.com/toksikk/gbp-gamerstatus v0.1.4
+	golang.org/x/crypto v0.16.0 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 )
--- pkg/gbploader/gbploader.go
+++ pkg/gbploader/gbploader.go
@@ -4,8 +4,15 @@ import (
 	"log/slog"
 	"path/filepath"
 	"plugin"
+	"runtime/debug"
 
 	"github.com/bwmarrin/discordgo"
+	gbp_coffee "github.com/toksikk/gbp-coffee/plugin"
+	gbp_eso "github.com/toksikk/gbp-eso/plugin"
+	gbp_gamerstatus "github.com/toksikk/gbp-gamerstatus/plugin"
+	gbp_leetoclock "github.com/toksikk/gbp-leetoclock/plugin"
+	gbp_stoll "github.com/toksikk/gbp-stoll/plugin"
+	gbp_wttrin "github.com/toksikk/gbp-wttrin/plugin"
 )
 
 var pluginStarter interface { // nolint:unused
@@ -19,20 +26,69 @@ func GetLoadedPlugins() *map[string][2]string {
 	return &loadedPlugins
 }
 
+func loadLibraryPlugins(discord *discordgo.Session) {
+	buildInfo, ok := debug.ReadBuildInfo()
+	if !ok {
+		slog.Warn("Could not read build info.")
+	}
+
+	if ok {
+		for _, dep := range buildInfo.Deps {
+			if dep.Path == "github.com/toksikk/gbp-coffee" {
+				slog.Info("Loading built-in plugin.", "plugin", gbp_coffee.PluginName, "version", dep.Version)
+				addPluginToLoadedPlugins(gbp_coffee.PluginName, dep.Version, "compiled into Gidbig")
+			}
+			if dep.Path == "github.com/toksikk/gbp-gamerstatus" {
+				slog.Info("Loading built-in plugin.", "plugin", gbp_gamerstatus.PluginName, "version", dep.Version)
+				addPluginToLoadedPlugins(gbp_gamerstatus.PluginName, dep.Version, "compiled into Gidbig")
+			}
+			if dep.Path == "github.com/toksikk/gbp-wttrin" {
+				slog.Info("Loading built-in plugin.", "plugin", gbp_wttrin.PluginName, "version", dep.Version)
+				addPluginToLoadedPlugins(gbp_wttrin.PluginName, dep.Version, "compiled into Gidbig")
+			}
+			if dep.Path == "github.com/toksikk/gbp-leetoclock" {
+				slog.Info("Loading built-in plugin.", "plugin", gbp_leetoclock.PluginName, "version", dep.Version)
+				addPluginToLoadedPlugins(gbp_leetoclock.PluginName, dep.Version, "compiled into Gidbig")
+			}
+			if dep.Path == "github.com/toksikk/gbp-eso" {
+				slog.Info("Loading built-in plugin.", "plugin", gbp_eso.PluginName, "version", dep.Version)
+				addPluginToLoadedPlugins(gbp_eso.PluginName, dep.Version, "compiled into Gidbig")
+			}
+			if dep.Path == "github.com/toksikk/gbp-stoll" {
+				slog.Info("Loading built-in plugin.", "plugin", gbp_stoll.PluginName, "version", dep.Version)
+				addPluginToLoadedPlugins(gbp_stoll.PluginName, dep.Version, "compiled into Gidbig")
+			}
+		}
+	} else {
+		addPluginToLoadedPlugins(gbp_coffee.PluginName, "unknown version", "compiled into Gidbig")
+		addPluginToLoadedPlugins(gbp_gamerstatus.PluginName, "unknown version", "compiled into Gidbig")
+		addPluginToLoadedPlugins(gbp_wttrin.PluginName, "unknown version", "compiled into Gidbig")
+		addPluginToLoadedPlugins(gbp_leetoclock.PluginName, "unknown version", "compiled into Gidbig")
+		addPluginToLoadedPlugins(gbp_eso.PluginName, "unknown version", "compiled into Gidbig")
+		addPluginToLoadedPlugins(gbp_stoll.PluginName, "unknown version", "compiled into Gidbig")
+	}
+
+	gbp_coffee.Start(discord)
+	gbp_gamerstatus.Start(discord)
+	gbp_wttrin.Start(discord)
+}
+
+func addPluginToLoadedPlugins(pluginName string, pluginVersion string, pluginBuilddate string) {
+	loadedPlugins[pluginName] = [2]string{pluginVersion, pluginBuilddate}
+}
+
 // LoadPlugins from plugins directory
-func LoadPlugins(discord *discordgo.Session) {
+func loadBinaryPlugins(discord *discordgo.Session) {
 	plugins, err := filepath.Glob("./plugins/*.so")
 	if err != nil {
 		slog.Warn("Could not load plugins.", "error", err)
 		return
 	}
 
-	loadedPlugins = make(map[string][2]string)
-
 	for _, v := range plugins {
 		plugin, err := plugin.Open(v)
 		if err != nil {
-			slog.Warn("Could not open plugin.", "plugin", plugin, "error", err)
+			slog.Warn("Could not open plugin or plugin was already loaded as built-in.", "plugin", plugin, "error", err)
 			continue
 		}
 
@@ -65,4 +121,11 @@ func LoadPlugins(discord *discordgo.Session) {
 
 		startFunc.(func(*discordgo.Session))(discord)
 	}
+}
+
+// LoadPlugins from deps and from plugins directory
+func LoadPlugins(discord *discordgo.Session) {
+	loadedPlugins = make(map[string][2]string)
+	loadLibraryPlugins(discord)
+	loadBinaryPlugins(discord)
 }
