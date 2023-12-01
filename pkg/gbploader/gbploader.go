cc7ebddb4e820a63285b49b1d479c6806f8bc84b
Daniel Aberger <da@ixab.de>
2023-12-01 12:17:54 +0100

    feat(plugins): prioritize binary plugins over libs

--- pkg/gbploader/gbploader.go
+++ pkg/gbploader/gbploader.go
@@ -35,45 +35,93 @@ func loadLibraryPlugins(discord *discordgo.Session) {
 	if ok {
 		for _, dep := range buildInfo.Deps {
 			if dep.Path == "github.com/toksikk/gbp-coffee" {
+				if _, alreadyLoaded := loadedPlugins[gbp_coffee.PluginName]; alreadyLoaded {
+					slog.Info("Plugin already loaded from binary. Skipping...", "plugin", gbp_coffee.PluginName)
+					continue
+				}
 				slog.Info("Loading built-in plugin.", "plugin", gbp_coffee.PluginName, "version", dep.Version)
 				addPluginToLoadedPlugins(gbp_coffee.PluginName, dep.Version, "compiled into Gidbig")
 			}
 			if dep.Path == "github.com/toksikk/gbp-gamerstatus" {
+				if _, alreadyLoaded := loadedPlugins[gbp_gamerstatus.PluginName]; alreadyLoaded {
+					slog.Info("Plugin already loaded from binary. Skipping...", "plugin", gbp_gamerstatus.PluginName)
+					continue
+				}
 				slog.Info("Loading built-in plugin.", "plugin", gbp_gamerstatus.PluginName, "version", dep.Version)
 				addPluginToLoadedPlugins(gbp_gamerstatus.PluginName, dep.Version, "compiled into Gidbig")
 			}
 			if dep.Path == "github.com/toksikk/gbp-wttrin" {
+				if _, alreadyLoaded := loadedPlugins[gbp_wttrin.PluginName]; alreadyLoaded {
+					slog.Info("Plugin already loaded from binary. Skipping...", "plugin", gbp_wttrin.PluginName)
+					continue
+				}
 				slog.Info("Loading built-in plugin.", "plugin", gbp_wttrin.PluginName, "version", dep.Version)
 				addPluginToLoadedPlugins(gbp_wttrin.PluginName, dep.Version, "compiled into Gidbig")
 			}
 			if dep.Path == "github.com/toksikk/gbp-leetoclock" {
+				if _, alreadyLoaded := loadedPlugins[gbp_leetoclock.PluginName]; alreadyLoaded {
+					slog.Info("Plugin already loaded from binary. Skipping...", "plugin", gbp_leetoclock.PluginName)
+					continue
+				}
 				slog.Info("Loading built-in plugin.", "plugin", gbp_leetoclock.PluginName, "version", dep.Version)
 				addPluginToLoadedPlugins(gbp_leetoclock.PluginName, dep.Version, "compiled into Gidbig")
 			}
 			if dep.Path == "github.com/toksikk/gbp-eso" {
+				if _, alreadyLoaded := loadedPlugins[gbp_eso.PluginName]; alreadyLoaded {
+					slog.Info("Plugin already loaded from binary. Skipping...", "plugin", gbp_eso.PluginName)
+					continue
+				}
 				slog.Info("Loading built-in plugin.", "plugin", gbp_eso.PluginName, "version", dep.Version)
 				addPluginToLoadedPlugins(gbp_eso.PluginName, dep.Version, "compiled into Gidbig")
 			}
 			if dep.Path == "github.com/toksikk/gbp-stoll" {
+				if _, alreadyLoaded := loadedPlugins[gbp_stoll.PluginName]; alreadyLoaded {
+					slog.Info("Plugin already loaded from binary. Skipping...", "plugin", gbp_stoll.PluginName)
+					continue
+				}
 				slog.Info("Loading built-in plugin.", "plugin", gbp_stoll.PluginName, "version", dep.Version)
 				addPluginToLoadedPlugins(gbp_stoll.PluginName, dep.Version, "compiled into Gidbig")
 			}
 		}
 	} else {
-		addPluginToLoadedPlugins(gbp_coffee.PluginName, "unknown version", "compiled into Gidbig")
-		addPluginToLoadedPlugins(gbp_gamerstatus.PluginName, "unknown version", "compiled into Gidbig")
-		addPluginToLoadedPlugins(gbp_wttrin.PluginName, "unknown version", "compiled into Gidbig")
-		addPluginToLoadedPlugins(gbp_leetoclock.PluginName, "unknown version", "compiled into Gidbig")
-		addPluginToLoadedPlugins(gbp_eso.PluginName, "unknown version", "compiled into Gidbig")
-		addPluginToLoadedPlugins(gbp_stoll.PluginName, "unknown version", "compiled into Gidbig")
+		if _, alreadyLoaded := loadedPlugins[gbp_coffee.PluginName]; alreadyLoaded {
+			addPluginToLoadedPlugins(gbp_coffee.PluginName, "unknown version", "compiled into Gidbig")
+		}
+		if _, alreadyLoaded := loadedPlugins[gbp_gamerstatus.PluginName]; alreadyLoaded {
+			addPluginToLoadedPlugins(gbp_gamerstatus.PluginName, "unknown version", "compiled into Gidbig")
+		}
+		if _, alreadyLoaded := loadedPlugins[gbp_wttrin.PluginName]; alreadyLoaded {
+			addPluginToLoadedPlugins(gbp_wttrin.PluginName, "unknown version", "compiled into Gidbig")
+		}
+		if _, alreadyLoaded := loadedPlugins[gbp_leetoclock.PluginName]; alreadyLoaded {
+			addPluginToLoadedPlugins(gbp_leetoclock.PluginName, "unknown version", "compiled into Gidbig")
+		}
+		if _, alreadyLoaded := loadedPlugins[gbp_eso.PluginName]; alreadyLoaded {
+			addPluginToLoadedPlugins(gbp_eso.PluginName, "unknown version", "compiled into Gidbig")
+		}
+		if _, alreadyLoaded := loadedPlugins[gbp_stoll.PluginName]; alreadyLoaded {
+			addPluginToLoadedPlugins(gbp_stoll.PluginName, "unknown version", "compiled into Gidbig")
+		}
 	}
 
-	gbp_coffee.Start(discord)
-	gbp_gamerstatus.Start(discord)
-	gbp_wttrin.Start(discord)
-	gbp_leetoclock.Start(discord)
-	gbp_eso.Start(discord)
-	gbp_stoll.Start(discord)
+	if _, alreadyLoaded := loadedPlugins[gbp_coffee.PluginName]; alreadyLoaded {
+		gbp_coffee.Start(discord)
+	}
+	if _, alreadyLoaded := loadedPlugins[gbp_gamerstatus.PluginName]; alreadyLoaded {
+		gbp_gamerstatus.Start(discord)
+	}
+	if _, alreadyLoaded := loadedPlugins[gbp_wttrin.PluginName]; alreadyLoaded {
+		gbp_wttrin.Start(discord)
+	}
+	if _, alreadyLoaded := loadedPlugins[gbp_leetoclock.PluginName]; alreadyLoaded {
+		gbp_leetoclock.Start(discord)
+	}
+	if _, alreadyLoaded := loadedPlugins[gbp_eso.PluginName]; alreadyLoaded {
+		gbp_eso.Start(discord)
+	}
+	if _, alreadyLoaded := loadedPlugins[gbp_stoll.PluginName]; alreadyLoaded {
+		gbp_stoll.Start(discord)
+	}
 }
 
 func addPluginToLoadedPlugins(pluginName string, pluginVersion string, pluginBuilddate string) {
@@ -91,7 +139,7 @@ func loadBinaryPlugins(discord *discordgo.Session) {
 	for _, v := range plugins {
 		plugin, err := plugin.Open(v)
 		if err != nil {
-			slog.Warn("Could not open plugin or plugin was already loaded as built-in.", "plugin", plugin, "error", err)
+			slog.Warn("Could not open plugin.", "plugin", plugin, "error", err)
 			continue
 		}
 
@@ -129,6 +177,6 @@ func loadBinaryPlugins(discord *discordgo.Session) {
 // LoadPlugins from deps and from plugins directory
 func LoadPlugins(discord *discordgo.Session) {
 	loadedPlugins = make(map[string][2]string)
-	loadLibraryPlugins(discord)
 	loadBinaryPlugins(discord)
+	loadLibraryPlugins(discord)
 }
