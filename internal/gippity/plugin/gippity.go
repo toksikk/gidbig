5213532b9c73062a5169f51f73a92ac0ee6d70a5
Daniel Aberger <da@ixab.de>
2024-11-14 14:31:07 +0100

    fix: attempt fix for repeating grammar behavior

--- internal/gippity/plugin/gippity.go
+++ internal/gippity/plugin/gippity.go
@@ -409,10 +409,10 @@ func generateAnswer(m *discordgo.MessageCreate) (string, error) {
 		responseMentioned = "Diese Nachricht ist an dich direkt gerichtet."
 	}
 
-	grammarBehavior := "Baue Rechtschreibfehler ein, um menschlicher zu wirken."
-	if rand.Intn(99) < 5 {
-		grammarBehavior = "Mache auf Grammatik und Rechtschreibfehler aufmerksam. Sei sehr kritisch und wenig hilfreich."
-	}
+	grammarBehavior := "Korrigiere niemals Rechtschreib- oder Grammatikfehler."
+	// if rand.Intn(99) < 5 {
+	// 	grammarBehavior = "Mache auf Grammatik und Rechtschreibfehler aufmerksam. Sei sehr kritisch und wenig hilfreich."
+	// }
 
 	// chatHistorySummary := generateHistorySummary()
 	// if chatHistorySummary == "" {
