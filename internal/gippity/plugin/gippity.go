3725beccc59fc503b87b8e5db618285569ff3afd
Daniel Aberger <da@ixab.de>
2024-12-23 23:07:29 +0100

    fix: newest user message now has timestamp again

--- internal/gippity/plugin/gippity.go
+++ internal/gippity/plugin/gippity.go
@@ -216,7 +216,10 @@ func generateAnswer(m *discordgo.MessageCreate) (string, error) {
 		}
 	}
 
-	messages = append(messages, openai.ChatCompletionMessageParamUnion(openai.UserMessage(replaceAllUserIDsWithUsernamesInStringMessage(m.Message.Content, m.GuildID))))
+	// TODO: this could potentially if we chose to no include user ids in message later
+	messages = append(messages, openai.ChatCompletionMessageParamUnion(openai.UserMessage(
+		replaceAllUserIDsWithUsernamesInStringMessage(
+			convertStringMessageToLLMCompatibleFlowingText(m.Content, m.Author.Username, m.Timestamp.Format("2006-01-02 15:04:05")), m.GuildID))))
 
 	chatCompletion, err := openaiClient.Chat.Completions.New(context.TODO(), openai.ChatCompletionNewParams{
 		Messages: openai.F(messages),
