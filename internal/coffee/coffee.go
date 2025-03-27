03d49c7affe9dd495f8ffb2597f875a67c5283f0
Daniel Aberger <da@ixab.de>
2025-03-27 09:44:51 +0100

    refactor: simplify reaction logic and move constants to util package

--- internal/coffee/coffee.go
+++ internal/coffee/coffee.go
@@ -3,7 +3,6 @@ package coffee
 import (
 	"log/slog"
 	"strings"
-	"time"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/toksikk/gidbig/internal/util"
@@ -30,10 +29,6 @@ var messages = []string{
 	"christkindl",
 }
 
-var cl []byte = []byte{0xF0, 0x9F, 0xA4, 0xA1}
-var da byte = byte(0b00100101) ^ byte(0x13) ^ byte(0x37)
-var mo byte = byte(0b00100000) ^ byte(0x13) ^ byte(0x37)
-
 // Start the plugin
 func Start(discord *discordgo.Session) {
 	discord.AddHandler(onMessageCreate)
@@ -43,9 +38,8 @@ func Start(discord *discordgo.Session) {
 func onMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	for _, v := range messages {
 		if v == strings.ToLower(m.Content) {
-			today := time.Now()
-			if today.Day() == int(da) && today.Month() == time.Month(int(mo)) {
-				util.ReactOnMessage(s, m.ChannelID, m.ID, string(cl), "add")
+			if util.IsSpecial() {
+				util.ReactOnMessage(s, m.ChannelID, m.ID, string(util.Cl), "add")
 			} else {
 				if m.Author.ID == "263959699764805642" || m.Author.ID == "217697101818232832" {
 					util.ReactOnMessage(s, m.ChannelID, m.ID, "🍵", "add")
--- internal/util/util.go
+++ internal/util/util.go
@@ -5,8 +5,19 @@ import (
 	"time"
 )
 
+// Cl is a byte slice containing the bytes for something special
+var Cl []byte = []byte{0xF0, 0x9F, 0xA4, 0xA1}
+var da byte = byte(0b00100101) ^ byte(0x13) ^ byte(0x37)
+var mo byte = byte(0b00100000) ^ byte(0x13) ^ byte(0x37)
+
 // RandomRange returns a random integer between min and max
 func RandomRange(min, max int) int {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	return r.Intn(max-min) + min
+}
+
+// IsSpecial returns true if today is a special day
+func IsSpecial() bool {
+	today := time.Now()
+	return today.Day() == int(da) && today.Month() == time.Month(int(mo))
 }
