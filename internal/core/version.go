c51e1fc09daeb22577a7cd90c602b2c67b14cf09
Daniel Aberger <da@ixab.de>
2026-08-31 14:12:01 +0200

    status: satisfy commit hash lint check

--- internal/core/version.go
+++ internal/core/version.go
@@ -44,7 +44,7 @@ func isCommitHash(s string) bool {
 		return false
 	}
 	for _, r := range s {
-		if !(r >= '0' && r <= '9') && !(r >= 'a' && r <= 'f') {
+		if (r < '0' || r > '9') && (r < 'a' || r > 'f') {
 			return false
 		}
 	}
